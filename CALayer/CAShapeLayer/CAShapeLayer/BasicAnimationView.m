174700e194811f36ab803bdad9fb49f4fa95513b
felix <2319979647@qq.com>
2016-12-28 15:14:13 +0800

    fix

--- CALayer/CAShapeLayer/CAShapeLayer/BasicAnimationView.m
+++ CALayer/CAShapeLayer/CAShapeLayer/BasicAnimationView.m
@@ -68,6 +68,8 @@
  
     //1.创建动画并指定动画属性
     CABasicAnimation *basicAnimation=[CABasicAnimation animationWithKeyPath:@"position"];
+//    basicAnimation setValue:kCAMediaTimingFunctionEaseInEaseOut forKey:<#(nonnull NSString *)#>
+    [basicAnimation.timingFunction setValue:kCAMediaTimingFunctionEaseOut forKeyPath:@"position"];
     basicAnimation.delegate = self;
     //2.设置动画属性初始值和结束值
     basicAnimation.fromValue=[NSValue valueWithCGPoint:CGPointMake(50, 150)];//可以不设置，默认为图层初始状态
@@ -82,7 +84,6 @@
     [_layer addAnimation:basicAnimation forKey:@"KCBasicAnimation_Translation"]; //(la1) layer 添加animation
     
     [basicAnimation setValue:[NSValue valueWithCGPoint:location] forKey:@"KCBasicAnimationLocation"];
-
 
 }
 
@@ -123,7 +124,6 @@
     
     NSLog(@"animation(%@) start.\r_layer.frame=%@",anim,NSStringFromCGRect(_layer.frame));
     NSLog(@"%@",[_layer animationForKey:@"KCBasicAnimation_Translation"]);//通过前面的设置的key获得动画
-    
    
 }
 
@@ -170,9 +170,10 @@
  2~3 ： 这个之间可以自定义设置其他的属性
  3、给layer添加动画 （layer可能也会添加很多个动画，所以也会有key值）
  
- &&
+ &&&&
  (la1:表示使用layer 和animaiton 关系的第一个)
  
+ (1)&&&&&
  ???
  (1) translatonAnimation 平移的方法处理之后出现了平移了之后又会回到原来起始的位置，怎么破????
  前面说过图层动画的本质就是将图层内部的内容转化为位图经硬件操作形成一种动画效果，其实图层本身并没有任何的变化.上面的动画中图层并没有因为动画效果而改变它的位置（对于缩放动画其大小也是不会改变的）所以动画完成之后图层还是在原来的显示位置没有任何变化，如果这个图层在一个UIView中你会发现在UIView移动过程中你要触发UIView的点击事件也只能点击原来的位置（即使它已经运动到了别的位置），因为它的位置从来没有变过。
@@ -189,6 +190,13 @@
  
  &&&&&
 CATransaction 就是一个事务：（控制动画的动画过程中的显示动画还是隐式动画的控制）
+ CATransaction 是coreAnimation上的一个机制，用于配置 多个layer图层的操作进入原子更新头渲染树种。每一个对layer tree的修改都要求事务成为其中的一部分。
+ 
+ coreAnimation有两种类型，一种是显示（明确） 一种隐式的（蕴含的）
+ 显示的是：通过 [CATransaction begin]方法调用的开始，在layer tree修改之前，[CATransaction commit] 这里结束。
+ 
+ 而隐式的则会自动在core animation 中自动创建，。layer tree修改的时候不会通过一个线程启动transation，他们会自动添加到run loop 线程中，他们自动提交当线程run loop 到下一个block中。这个也许需要使用精确的失误去
+ 
  这个对应的commit的内容是和run loop 有关的
  有关和兴的关键字
 CA_EXTERN NSString * const kCATransactionAnimationDuration // 事务的动画时间
@@ -209,8 +217,71 @@ CA_AVAILABLE_STARTING (10.6, 4.0, 9.0, 2.0);
 // 在多线程中需要进行处理这个国车用
 + (void)lock;
 + (void)unlock;
+ 
+ //block 类型
+ #if __BLOCKS__
+ + (nullable void (^)(void))completionBlock;
+ + (void)setCompletionBlock:(nullable void (^)(void))block;
+ #endif
+ 
+ + (nullable id)valueForKey:(NSString *)key;
+ + (void)setValue:(nullable id)anObject forKey:(NSString *)key;
+ 
+ "animationDuration", "animationTimingFunction", "completionBlock",
+ * "disableActions".
+ 
+ PS:(总结)
+ 为什么这个还是不行呢？
+ 因为动画已经执行完了，就会返回到上面的那个点，所以，要设置这个点，这个时候也会调到最后的位置，也是有问题的
 #&&&&&
  
+(2)&&&& 控制时间段动画
+ CAMediaTimingFunction  (可以翻译为： 媒体时间段行为)
+ NSKeyValueCoding kvc的接口访问 （KVC的具体内容）
+
+ 这里使用媒体汗水类来进行管理
+ Timing function names.
+CA_EXTERN NSString * const kCAMediaTimingFunctionLinear  //线性
+CA_AVAILABLE_STARTING (10.5, 2.0, 9.0, 2.0);
+CA_EXTERN NSString * const kCAMediaTimingFunctionEaseIn   //  开始的时候缓慢
+CA_AVAILABLE_STARTING (10.5, 2.0, 9.0, 2.0);
+CA_EXTERN NSString * const kCAMediaTimingFunctionEaseOut  // 结束的时候缓慢
+CA_AVAILABLE_STARTING (10.5, 2.0, 9.0, 2.0);
+CA_EXTERN NSString * const kCAMediaTimingFunctionEaseInEaseOut  // 开始和结束的回收偶读缓慢
+CA_AVAILABLE_STARTING (10.5, 2.0, 9.0, 2.0);
+CA_EXTERN NSString * const kCAMediaTimingFunctionDefault   // 默认的
+CA_AVAILABLE_STARTING (10.6, 3.0, 9.0, 2.0);
+
+ 这个应该是一个相关的内容，也就是时间上的行额能力；
+ + (instancetype):(NSString *)name;
+ 创建这样的一个对象，也就是如何进行创建对象
  
-  */
+  Creates a timing function modelled on a cubic Bezier curve. The end
+ * points of the curve are at (0,0) and (1,1), the two points 'c1' and
+ * 'c2' defined by the class instance are the control points. Thus the
+ * points defining the Bezier curve are: '[(0,0), c1, c2, (1,1)]'
++ (instancetype)functionWithControlPoints:(float)c1x :(float)c1y :(float)c2x :(float)c2y;
+- (instancetype)initWithControlPoints:(float)c1x :(float)c1y :(float)c2x :(float)c2y;
+这个方法就可以定义两个不同阶段的时间段的行为
+这里就是从c1到c2的动作。
+
+'idx' is a value from 0 to 3 inclusive.
+- (void)getControlPointAtIndex:(size_t)idx values:(float[2])ptr;
+
+这里还会涉及都value和point之间的数据结构的转化； 这个同样会涉及到CI或者UI的内容;(这个不是重点)
+
+(3) CAAnimation的动画的 父类
+ CAAnimationDelegate
+  - (void)animationDidStart:(CAAnimation *)anim;
+ 动画的开始就会调用
+ - (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag;
+ 动画结束会调用
+
+ CAAnimation 是一个抽象类，可以创建，protocol才不可以创建，不过创建了需要进行定义了一些对应的内容，一般都是不用自定义创建，一般是写子类来进程这个类，我们就可以使用这个类的方法
+testAnimaitonAttributes 上面的这个方法就写了有关属性的测试
+ 基本上就是打通了：mediaTimingFunction / layer （以及transaction） 的关系；
+ 
+ （4）CALayer 图层Layer子类
+ 
+*/
 
