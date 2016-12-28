d6e79708ff4654c185eb48896333e5b60271b55c
felix <2319979647@qq.com>
2016-12-28 19:26:57 +0800

    fix

---
+++ CALayer/CAShapeLayer/CAShapeLayer/KeyFrameAnimationView.h
@@ -0,0 +1,13 @@
+//
+//  KeyFrameAnimationView.h
+//  CAShapeLayer
+//
+//  Created by felix on 2016/12/28.
+//  Copyright © 2016年 felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyFrameAnimationView : UIView
+
+@end
---
+++ CALayer/CAShapeLayer/CAShapeLayer/KeyFrameAnimationView.m
@@ -0,0 +1,14 @@
+
+//
+//  KeyFrameAnimationView.m
+//  CAShapeLayer
+//
+//  Created by felix on 2016/12/28.
+//  Copyright © 2016年 felix. All rights reserved.
+//
+
+#import "KeyFrameAnimationView.h"
+
+@implementation KeyFrameAnimationView
+
+@end
