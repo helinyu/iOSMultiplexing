d84e1b1931486829ada9f8e0d5f2e33f2ebf7f30
felix <2319979647@qq.com>
2016-12-30 09:25:38 +0800

    fix CAShapeLayer bug of width number

--- CALayer/CAShapeLayer/CAShapeLayer/ShareLayerView.m
+++ CALayer/CAShapeLayer/CAShapeLayer/ShareLayerView.m
@@ -17,9 +17,9 @@
 
 @end
 
-const CGFloat LayerWidth = 20.0f;
+#define Screen_width [[UIScreen mainScreen] bounds].size.width
 const CGFloat LayerHeight = 20.0f;
-const CGFloat PistionY = 30.0f;
+const CGFloat PistionY = 100.0f;
 
 @implementation ShareLayerView
 
@@ -28,9 +28,13 @@ const CGFloat PistionY = 30.0f;
     self = [super initWithFrame:frame];
     if (self) {
         
+        UIView *backView = [[UIView alloc] initWithFrame:CGRectMake(0, 100, [[UIScreen mainScreen] bounds].size.width, LayerHeight)];
+        backView.backgroundColor = [UIColor greenColor];
+        [self addSubview:backView];
+        
         _path = [UIBezierPath bezierPath];
-        [_path moveToPoint:CGPointMake(0,  PistionY)];
-        [_path addLineToPoint:CGPointMake(LayerWidth,PistionY)];
+        [_path moveToPoint:CGPointMake(0,PistionY+10)];
+        [_path addLineToPoint:CGPointMake(Screen_width,PistionY+10)];
 
         self.backgroundColor = [UIColor grayColor];
         CAShapeLayer *progressLayer = [CAShapeLayer layer];
@@ -43,93 +47,96 @@ const CGFloat PistionY = 30.0f;
         [self.layer addSublayer:progressLayer];
         
         [self renderProgress];
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            progressLayer.strokeEnd = 1.0f;
+        });
     }
     return self;
 }
 
-- (void)drawRect:(CGRect)rect {
-    //create path
-    UIBezierPath *path = [[UIBezierPath alloc] init];
-    [path moveToPoint:CGPointMake(175, 100)];
-    
-    [path addArcWithCenter:CGPointMake(150, 100) radius:25 startAngle:0 endAngle:2*M_PI clockwise:YES];
-    [path moveToPoint:CGPointMake(150, 125)];
-    [path addLineToPoint:CGPointMake(150, 175)];
-    [path addLineToPoint:CGPointMake(125, 225)];
-    [path moveToPoint:CGPointMake(150, 175)];
-    [path addLineToPoint:CGPointMake(175, 225)];
-    [path moveToPoint:CGPointMake(100, 150)];
-    [path addLineToPoint:CGPointMake(200, 150)];
-    
-    //create shape layer
-    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
-    shapeLayer.strokeColor = [UIColor redColor].CGColor;
-    shapeLayer.fillColor = [UIColor clearColor].CGColor;// 填充空区域，笔画中油连带，有弯曲的地方就会填充颜色
-    shapeLayer.lineWidth = 20;
-    shapeLayer.lineJoin = kCALineJoinMiter; // 也即是斜接的时候是接着的（两个线段之间的关系）
-    shapeLayer.lineCap = kCALineCapRound; //  设置角是圆的
-    shapeLayer.path = path.CGPath;
-    shapeLayer.miterLimit = 0;
-    
-    UIBezierPath *namePath = [UIBezierPath new];
-    [namePath moveToPoint:CGPointMake(20, 200)];
-    [namePath addLineToPoint:CGPointMake(60, 200)];
-    [namePath moveToPoint:CGPointMake(20, 200)];
-    [namePath addLineToPoint:CGPointMake(20, 350)];
-    [namePath moveToPoint:CGPointMake(20, 260)];
-    [namePath addLineToPoint:CGPointMake(55, 260)];
-    // F
-    
-    // E
-    [namePath moveToPoint:CGPointMake(80, 260)];
-    [namePath addLineToPoint:CGPointMake(130, 260)];
-    [namePath addArcWithCenter:CGPointMake(100, 260) radius:30.0f startAngle:0 endAngle:((M_PI/8)-(M_PI*2)) clockwise:false];
-// 这里需要上下翻转一下
-    
-    // L
-    [namePath moveToPoint:CGPointMake(140, 200)];
-    [namePath addLineToPoint:CGPointMake(140, 350)];
-    [namePath addLineToPoint:CGPointMake(210, 350)];
-    
-    // I
-    [namePath moveToPoint:CGPointMake(220, 200)];
-    [namePath addLineToPoint:CGPointMake(220, 350)];
-    
-    // X
-    [namePath moveToPoint:CGPointMake(230, 200)];
-    [namePath addLineToPoint:CGPointMake(280, 350)];
-    [namePath moveToPoint:CGPointMake(280, 200)];
-    [namePath addLineToPoint:CGPointMake(230, 350)];
-    
-    
-    CAShapeLayer *nameLayer = [CAShapeLayer layer];
-    nameLayer.strokeColor = [UIColor blueColor].CGColor;
-    nameLayer.fillColor = [UIColor clearColor].CGColor;
-    nameLayer.lineWidth = 5.0f;
-    nameLayer.lineCap = kCALineCapSquare;
-    nameLayer.lineJoin = kCALineJoinMiter;
-    nameLayer.path = namePath.CGPath;
-    
-//    self.progressLayer.frame = CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, 20);
-//    self.progressLayer.strokeColor = [UIColor redColor].CGColor;
-//    self.progressLayer.fillColor = [UIColor greenColor].CGColor;
-//    [self.layer addSublayer:self.progressLayer];
-    
-//    [self.layer addSublayer:nameLayer];
-
-    //add it to our view
-//    [self.layer addSublayer:shapeLayer];
-}
+//- (void)drawRect:(CGRect)rect {
+//    //create path
+//    UIBezierPath *path = [[UIBezierPath alloc] init];
+//    [path moveToPoint:CGPointMake(175, 100)];
+//    
+//    [path addArcWithCenter:CGPointMake(150, 100) radius:25 startAngle:0 endAngle:2*M_PI clockwise:YES];
+//    [path moveToPoint:CGPointMake(150, 125)];
+//    [path addLineToPoint:CGPointMake(150, 175)];
+//    [path addLineToPoint:CGPointMake(125, 225)];
+//    [path moveToPoint:CGPointMake(150, 175)];
+//    [path addLineToPoint:CGPointMake(175, 225)];
+//    [path moveToPoint:CGPointMake(100, 150)];
+//    [path addLineToPoint:CGPointMake(200, 150)];
+//    
+//    //create shape layer
+//    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+//    shapeLayer.strokeColor = [UIColor redColor].CGColor;
+//    shapeLayer.fillColor = [UIColor clearColor].CGColor;// 填充空区域，笔画中油连带，有弯曲的地方就会填充颜色
+//    shapeLayer.lineWidth = 20;
+//    shapeLayer.lineJoin = kCALineJoinMiter; // 也即是斜接的时候是接着的（两个线段之间的关系）
+//    shapeLayer.lineCap = kCALineCapRound; //  设置角是圆的
+//    shapeLayer.path = path.CGPath;
+//    shapeLayer.miterLimit = 0;
+//    
+//    UIBezierPath *namePath = [UIBezierPath new];
+//    [namePath moveToPoint:CGPointMake(20, 200)];
+//    [namePath addLineToPoint:CGPointMake(60, 200)];
+//    [namePath moveToPoint:CGPointMake(20, 200)];
+//    [namePath addLineToPoint:CGPointMake(20, 350)];
+//    [namePath moveToPoint:CGPointMake(20, 260)];
+//    [namePath addLineToPoint:CGPointMake(55, 260)];
+//    // F
+//    
+//    // E
+//    [namePath moveToPoint:CGPointMake(80, 260)];
+//    [namePath addLineToPoint:CGPointMake(130, 260)];
+//    [namePath addArcWithCenter:CGPointMake(100, 260) radius:30.0f startAngle:0 endAngle:((M_PI/8)-(M_PI*2)) clockwise:false];
+//// 这里需要上下翻转一下
+//    
+//    // L
+//    [namePath moveToPoint:CGPointMake(140, 200)];
+//    [namePath addLineToPoint:CGPointMake(140, 350)];
+//    [namePath addLineToPoint:CGPointMake(210, 350)];
+//    
+//    // I
+//    [namePath moveToPoint:CGPointMake(220, 200)];
+//    [namePath addLineToPoint:CGPointMake(220, 350)];
+//    
+//    // X
+//    [namePath moveToPoint:CGPointMake(230, 200)];
+//    [namePath addLineToPoint:CGPointMake(280, 350)];
+//    [namePath moveToPoint:CGPointMake(280, 200)];
+//    [namePath addLineToPoint:CGPointMake(230, 350)];
+//    
+//    
+//    CAShapeLayer *nameLayer = [CAShapeLayer layer];
+//    nameLayer.strokeColor = [UIColor blueColor].CGColor;
+//    nameLayer.fillColor = [UIColor clearColor].CGColor;
+//    nameLayer.lineWidth = 5.0f;
+//    nameLayer.lineCap = kCALineCapSquare;
+//    nameLayer.lineJoin = kCALineJoinMiter;
+//    nameLayer.path = namePath.CGPath;
+//    
+////    self.progressLayer.frame = CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, 20);
+////    self.progressLayer.strokeColor = [UIColor redColor].CGColor;
+////    self.progressLayer.fillColor = [UIColor greenColor].CGColor;
+////    [self.layer addSublayer:self.progressLayer];
+//    
+////    [self.layer addSublayer:nameLayer];
+//
+//    //add it to our view
+////    [self.layer addSublayer:shapeLayer];
+//}
 
 - (void)renderProgress {
        CABasicAnimation* strokeEndAnimation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-        strokeEndAnimation.duration = 30;
-        strokeEndAnimation.fromValue = @(0);
-        strokeEndAnimation.toValue = @(CGRectGetWidth([UIScreen mainScreen].bounds));
+        strokeEndAnimation.duration = 3.0;
+        strokeEndAnimation.fromValue = @(0.0);
+        strokeEndAnimation.toValue = @(1.0);
         strokeEndAnimation.autoreverses = NO;
         strokeEndAnimation.repeatCount = 0.0f;
         strokeEndAnimation.delegate = self;
-    
         [_progressLayer addAnimation:strokeEndAnimation forKey:@"strokeEndAnimation"];
 }
 
@@ -146,7 +153,14 @@ const CGFloat PistionY = 30.0f;
  * without being removed. */
 
 - (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
-    NSLog(@"animationDidStop");
+    
+//    NSLog(@"animationDidStop");
+//    [CATransaction begin];
+//    [CATransaction setDisableActions:YES];
+//    self.progressLayer.strokeEnd = 1.0f;
+//    [CATransaction setDisableActions:NO];
+//    [CATransaction commit];
+    
 }
 
 @end
