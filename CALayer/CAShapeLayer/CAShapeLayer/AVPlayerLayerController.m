f37eecde913ede43bc6f5338095e2f812affe220
felix <2319979647@qq.com>
2017-01-05 18:52:32 +0800

    fix something attributes

--- CALayer/CAShapeLayer/CAShapeLayer/AVPlayerLayerController.m
+++ CALayer/CAShapeLayer/CAShapeLayer/AVPlayerLayerController.m
@@ -65,4 +65,24 @@
 我们用代码创建了一个AVPlayerLayer，但是我们仍然把它添加到了一个容器视图中，而不是直接在controller中的主视图上添加。这样其实是为了可以使用自动布局限制使得图层在最中间；否则，一旦设备被旋转了我们就要手动重新放置位置，因为Core Animation并不支持自动大小和自动布局（见第三章『图层几何学』）。
 
  因为AVPlayerLayer是CALayer的子类，它继承了父类的所有特性。我们并不会受限于要在一个矩形中播放视频；清单6.16演示了在3D，圆角，有色边框，蒙板，阴影等效果
+ 
+ + (AVPlayerLayer *)playerLayerWithPlayer:(nullable AVPlayer *)player;
+和AVPlayer是紧密连接起来的
+ 
+ @property (nonatomic, retain, nullable) AVPlayer *player;
+通过PlayerLayer可以读取对应的Player的内容
+ 
+ @property(copy) NSString *videoGravity;
+设置权重，就是显示的方式
+ 
+ @property(nonatomic, readonly, getter=isReadyForDisplay) BOOL readyForDisplay;
+是否准备播放
+ 
+ @property (nonatomic, readonly) CGRect videoRect NS_AVAILABLE(10_9, 7_0);
+video 的大小
+ 
+ @property (nonatomic, copy, nullable) NSDictionary<NSString *, id> *pixelBufferAttributes NS_AVAILABLE(10_11, 9_0);
+
+ mark：帧率和像素的处理
+ 
 */
