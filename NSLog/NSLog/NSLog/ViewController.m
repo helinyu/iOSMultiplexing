2243feeea3d1dc20c32e621cb2916784f6f221f7
felix <2319979647@qq.com>
2017-01-10 16:06:45 +0800

    fix rich log

--- NSLog/NSLog/NSLog/ViewController.m
+++ NSLog/NSLog/NSLog/ViewController.m
@@ -12,17 +12,41 @@
 
 @end
 
+#define CustomSelectorLog NSLog(@"selector :%@",NSStringFromSelector(_cmd))
+#define LogFunAndLine(object) NSLog(@"%s:%d object=%@",__func__,__LINE__,object)
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+//    [self test0];
+    [self customLog];
 }
 
+- (void)customLog {
+    NSLog(@"");
+    CustomSelectorLog;
+    NSMutableArray *someObject = [NSMutableArray array];
+    LogFunAndLine(someObject);
+    [someObject addObject:@"foo"];
+    LogFunAndLine(someObject);
+}
+
+- (void)test0 {
+    NSLog(@"selector :%@",NSStringFromSelector(_cmd)); // current method
+    NSLog(@"class : %@",NSStringFromClass([self class])); // current object's class
+    NSLog(@"file : %@ ",[[NSString stringWithUTF8String:__FILE__] lastPathComponent]); //Name of the source code file.
+    NSLog(@"call statck symbols : %@",[NSThread callStackSymbols]); //NSArray of the current stack trace as programmer-readable strings.这个一般是有崩溃的时候才会输出
+    NSMutableArray *someObject = [NSMutableArray array];
+    NSLog(@"%s:%d someObject=%@", __func__, __LINE__, someObject);
+    [someObject addObject:@"foo"];
+    NSLog(@"%s:%d someObject=%@", __func__, __LINE__, someObject);
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
